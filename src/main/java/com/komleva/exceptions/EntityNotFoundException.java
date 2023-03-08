eabb5d786956f8eff4e855085e54db2b8f436389
Kate Komleva <komlevakate99@gmail.com>
2023-03-08 11:34:39 +0300

    feat: add EntityNotFoundException

---
+++ src/main/java/com/komleva/exceptions/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package com.komleva.exceptions;
+
+public class EntityNotFoundException extends RuntimeException{
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
