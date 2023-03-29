edc8e5244ee0852c49aa54e8efb51d76b73560af
Kate Komleva <komlevakate99@gmail.com>
2023-03-29 22:35:24 +0300

    fix: deleted simple impl and frontController

--- src/main/java/com/komleva/Main.java
+++ src/main/java/com/komleva/Main.java
@@ -1,20 +1,13 @@
 package com.komleva;
 
-import com.komleva.configuration.DatabaseProperties;
-import com.komleva.domain.User;
-import com.komleva.repository.user.UserRepository;
-import com.komleva.repository.user.UserRepositoryImpl;
-
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        UserRepository userRepository = new UserRepositoryImpl(new DatabaseProperties());
-
-        System.out.println("\n Method findAll");
-        List<User> all = userRepository.findAll();
-        for (User user : all) {
-            System.out.println(user);
-        }
+//        UserRepository userRepository = new UserRepositoryImpl(new DatabaseProperties());
+//
+//        System.out.println("\n Method findAll");
+//        List<User> all = userRepository.findAll();
+//        for (User user : all) {
+//            System.out.println(user);
+//        }
     }
 }
